4e111854d3431fa6a1b14212b1735b2451582943
Bread Berry <breadberry@protonmail.com>
2021-10-14 23:34:00 +0200

    added structure

---
+++ src/app/components/game-master/game-master.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-game-master',
+  templateUrl: './game-master.component.html',
+  styleUrls: ['./game-master.component.sass']
+})
+export class GameMasterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
