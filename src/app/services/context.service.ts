2137f404290b18d27dd5e176fa7442c42c9ebe15
Bread Berry <breadberry@protonmail.com>
2021-10-15 15:41:04 +0200

    extended context service

--- src/app/services/context.service.ts
+++ src/app/services/context.service.ts
@@ -4,13 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ContextService {
-  private sampleText = 'Local residents had to flee their homes and schoolchildren ducked for cover under their desks as' +
+  private _activeText: string = 'Local residents had to flee their homes and schoolchildren ducked for cover under their desks as' +
       ' men armed with automatic rifles and rocket-propelled grenade launchers - believed to have been members of Shia ' +
-      'and Christian militias - exchanged fire in the streets.'
+      'and Christian militias - exchanged fire in the streets.';
+  private _activeTextWordCount: number = 28;
 
   constructor() {}
 
-  getText = () => {
-    return this.sampleText
+  public get text() {
+    return this._activeText;
+  }
+
+  public get wordCount() {
+    return this._activeTextWordCount;
+  }
+
+  public getNewText = (random: true, codingLanguage = "") => {
+    // TODO replace _activeText with new value, if random random.
+    // TODO create language ENUM and react appropriately.
   }
 }
