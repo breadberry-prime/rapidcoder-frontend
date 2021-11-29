d9b9c0f3c449e7e3f93e77774d41cdae8ef02602
yaron fanger <yaron.fanger@ylabs.ch>
2021-11-29 15:32:30 +0100

    added PressedLetter Interface | return type of observable

--- src/app/interfaces/pressed-letter.interface.ts
+++ src/app/interfaces/pressed-letter.interface.ts
@@ -1,5 +1,11 @@
+import {INPUT_STATE} from "../enums/input-state.enum";
+import {TEXT_PROGRESS_STATE} from "../enums/text-progress-state.enum";
+
 export interface PressedLetterInterface {
   letter: string,
-  isCorrect: boolean,
+  letterStatus: INPUT_STATE,
+  targetLetterIndex: number,
+  textStatus: TEXT_PROGRESS_STATE,
   isSpecialCharacter: boolean
 }
+
