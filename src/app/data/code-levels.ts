9ef26a10e0c648195d01562c28a39d50ff44252d
violin <florian.beqiraj@gmail.com>
2021-12-01 14:30:19 +0100

    edited code levels data to constant and optimized

--- src/app/data/code-levels.ts
+++ src/app/data/code-levels.ts
@@ -1,11 +1,11 @@
 import { CODE_LANGUAGE } from "../enums/code-language.enum";
 import { CodeLevelInterface } from "../interfaces/code-level.interface";
 
-export let codeLevels: CodeLevelInterface[] = [
+export const codeLevels: CodeLevelInterface[] = [
     {
         "level" : 1,
         "language" : CODE_LANGUAGE.PYTHON,
-        "content" : ">>> print(\"Hello World!\")"
+        "content" : "print(\"Hello World!\")"
     },
     {
         "level" : 1,
